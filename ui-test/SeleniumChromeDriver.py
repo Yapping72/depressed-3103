3c099fa381e2c52a51b15ef3f1f2fc63bea8e635
Yap Ping <Yapping72@gmail.com>
2023-11-15 11:57:27 +0800

    Add --disable-gpu flag.

--- ui-test/SeleniumChromeDriver.py
+++ ui-test/SeleniumChromeDriver.py
@@ -20,6 +20,7 @@ class SeleniumChromeDriver:
         chrome_options.add_argument('--disable-web-security')
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
         driver = webdriver.Chrome(options=chrome_options)
         return driver
 
